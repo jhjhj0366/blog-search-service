8c5037d2be499456c3702c13c655b6f6b1b55c3b
jeonghyunjeong <dev.jhjhj@gmail.com>
2022-07-02 19:17:03 +0900

    H2 DB Configuration

---
+++ src/main/java/com/search/blog/config/WebConfig.java
@@ -0,0 +1,35 @@
+package com.search.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public ClientHttpRequestFactory simpleClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setReadTimeout(2000);      // 2 sec.
+        factory.setConnectTimeout(3000);   // 3 sec.
+        return factory;
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("POST", "GET", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("Authorization", "Origin", "X-Requested-With", "Content-Type", "Accept")
+                .allowCredentials(false).maxAge(3600);
+    }
+}
